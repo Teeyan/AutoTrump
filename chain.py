5db02a8e3742ba4ca7dd541c0f9f41a8c0bdd8db
Teeyan <dantian00777@yahoo.com>
2018-05-15 23:57:05 -0500

    added chain creation and tweet generation

---
+++ chain.py
@@ -0,0 +1,32 @@
+import pickle
+import sys
+
+
+def generate_trigrams(words):
+    if len(words) < 3:
+        return
+    for i in range(0, len(words) - 2):
+        yield (words[i], words[i + 1], words[i + 2])
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Please input the text file location for chain generation")
+
+    filename = sys.argv[1]
+    chain = {}
+
+    with open(filename, "r") as fp:
+        for line in fp.readlines():
+            words = line.split()
+            for w1, w2, w3 in generate_trigrams(words):
+                key = (w1, w2)
+                if key in chain:
+                    chain[key].append(w3)
+                else:
+                    chain[key] = [w3]
+    pickle.dump(chain, open("markov_chain", "wb"))
+
+
+if __name__ == "__main__":
+    main()
---
+++ generator.py
@@ -0,0 +1,32 @@
+import pickle
+import random
+
+
+class TweetGenerator:
+
+    def __init__(self):
+        self.transition_dict = {}
+
+    def load_generator(self, filename):
+        """
+        Prep the generator by specifying the markov chain's transition probabilities.
+        :param filename: file location of the pickle object to be loaded
+        """
+        self.transition_dict = pickle.load(open(filename, "rb"))
+
+    def generate_tweet(self):
+        """
+        Generate a fake tweet based on the trigram model markov chain.
+        :return: string denoting the fake tweet
+        """
+        w1 = "BEGIN"
+        w2 = "TWEET"
+        tweet = ""
+        w3 = random.choice(self.transition_dict[(w1, w2)])
+        while w3 != "END":
+            tweet = tweet + " " + w3
+            w1 = w2
+            w2 = w3
+            w3 = random.choice(self.transition_dict[(w1, w2)])
+
+        return tweet
